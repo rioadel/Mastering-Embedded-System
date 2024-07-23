ec3ba2414328b36f7c5e17c6989505f8d7eb5466
Mario Adel <107910500+rioadel@users.noreply.github.com>
2024-07-23 06:05:40 +0300

    Example 1 Solution

---
+++ Unit_2_C_Programming/Unit_2_Lesson_3_C_Basics/Assignments/Assignment 1/EX1_Write C Program to Print a Sentence of C Programming.c
@@ -0,0 +1,13 @@
+/*
+ ============================================================================
+ Author      : Mario Adel
+ Description : Printing "C Programming" Sentence
+ ============================================================================
+ */
+
+#include <stdio.h>
+
+int main(void) {
+	printf("C Programming");
+	return 0;
+}
