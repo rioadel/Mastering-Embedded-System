533386ed3456e1322a498b92812fe9594b84f9cc
Mario Adel <107910500+rioadel@users.noreply.github.com>
2024-07-23 06:28:25 +0300

    Example 7 Solution

---
+++ Unit_2_C_Programming/Unit_2_Lesson_3_C_Basics/Assignments/Assignment 1/EX7_ Swapping Two Numbers using multiplying and division.c
@@ -0,0 +1,29 @@
+/*
+ ============================================================================
+ Author      : Mario Adel
+ Description : Swapping two numbers using multiplying and division
+ ============================================================================
+ */
+
+#include <stdio.h>
+
+int main(void) {
+	float a, b;
+	printf("Enter value of a: ");
+	fflush(stdout);
+	scanf("%f",&a);
+	fflush(stdin);
+	printf("Enter value of b: ");
+	fflush(stdout);
+	scanf("%f",&b);
+	fflush(stdin);
+	a = a * b;
+	b = a / b;
+	a = a / b;
+	printf("\nAfter swapping, value of a: %0.2f \n",a);
+	fflush(stdout);
+	printf("After swapping, value of b: %0.1f",b);
+	fflush(stdout);
+
+	return 0;
+}
